7638e31932f4ce79ed2a1f48eb57b3afc6d90fe6
Nick C <nick@partnersdentalstudio.com>
2026-01-16 11:23:10 -0600

    added grouping for charts

--- remake_external/essix remake investigation.sql
+++ remake_external/essix remake investigation.sql
@@ -23,7 +23,46 @@ WHERE
     AND main.Remake = 'Remake 100%'
     AND main.RemakeReason = 'Fit is poor'
     -- Product filter using ProductID
-    AND cp.ProductID IN ('essix 076', 'essix 1mm', 'essix 1.5', 'essix 2mm', 'Essix 3 Day', 'essix theroux','retain upper','retain lower')
+    ---- With Retain
+    --AND cp.ProductID IN ('essix 076', 'essix 1mm', 'essix 1.5', 'essix 2mm', 'Essix 3 Day', 'essix theroux','retain upper','retain lower')
+    ---- Without Retain
+    AND cp.ProductID IN ('essix 076', 'essix 1mm', 'essix 1.5', 'essix 2mm', 'Essix 3 Day', 'essix theroux')
+    ---- Only Retain
     --AND cp.ProductID IN ('retain upper')--,'retain lower')
     -- Link filter
     AND links.Notes LIKE '%Remake Of%';
+
+------------------------------------------------------------------------------
+---GROUPING for Charts
+------------------------------------------------------------------------------
+SELECT 
+    -- Date Grouping
+    FORMAT(main.DateIn, 'yyyy-MM') AS [YearMonth],
+    DATEPART(week, main.DateIn) AS [WeekNumber],
+    CAST(main.DateIn AS DATE) AS [ExactDate],
+    
+    -- Metrics
+    COUNT(main.CaseID) AS [RemakeCount],
+    
+    -- Keep categories for Excel Slicers (optional)
+    cp.ProductID AS [Product]
+FROM 
+    dbo.CaseLinks AS links
+INNER JOIN 
+    dbo.Cases AS main ON links.CaseID = main.CaseID
+INNER JOIN 
+    dbo.CaseProducts AS cp ON main.CaseID = cp.CaseID
+WHERE 
+    main.DateIn >= '2025-06-01'
+    AND main.Remake = 'Remake 100%'
+    AND main.RemakeReason = 'Fit is poor'
+    AND cp.ProductID IN ('essix 076', 'essix 1mm', 'essix 1.5', 'essix 2mm', 'Essix 3 Day', 'essix theroux','retain lower','retain upper')
+    --AND cp.ProductID IN ('retain lower','retain upper')
+    AND links.Notes LIKE '%Remake Of%'
+GROUP BY 
+    FORMAT(main.DateIn, 'yyyy-MM'),
+    DATEPART(week, main.DateIn),
+    CAST(main.DateIn AS DATE),
+    cp.ProductID
+ORDER BY 
+    [ExactDate] ASC;
